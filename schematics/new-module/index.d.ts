42e132dbbab79a8a17d12854683ce9c890d03d7e
wflixu <wflixu@yeah.net>
2020-09-06 11:51:52 +0800

    schematics module

---
+++ schematics/new-module/index.d.ts
@@ -0,0 +1,2 @@
+import { Rule } from '@angular-devkit/schematics';
+export declare function newModule(_options: any): Rule;
--- schematics/new-module/index.ts
+++ schematics/new-module/index.ts
@@ -1,10 +1,31 @@
-import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
+import { Rule, SchematicContext, Tree, SchematicsException } from '@angular-devkit/schematics';
+import { experimental } from '@angular-devkit/core';
 
 
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
 export function newModule(_options: any): Rule {
   return (tree: Tree, _context: SchematicContext) => {
+
+    const workspaceConfig = tree.read('/angular.json');
+    if (!workspaceConfig) {
+      throw new SchematicsException(
+        'Could not find Angular workspace configuration'
+      );
+    }
+
+    // convert workspace to string
+    const workspaceContent = workspaceConfig.toString();
+
+    // parse workspace string into JSON object
+    const workspace: experimental.workspace.WorkspaceSchema = JSON.parse(
+      workspaceContent
+    );
+    if (!_options.project) {
+      _options.project = 'lay';
+    }
+    console.log( _options.project);
+
     return tree;
   };
 }
